e89782d41cada0a275e37393a3eefb39cf0f52f9
lijinf2 <jinfeng@megagon.ai>
2020-02-11 01:01:48 +0000

    add cnn

---
+++ script/cnn.sh
@@ -0,0 +1,13 @@
+CUDA=1
+mkdir result
+echo "Dataset,precision,recall,f1,train_time(s),test_time(s)" > result/cnn.csv
+for dataset in "SUGG" 
+do
+    echo ${dataset}
+    DATA_DIR="../data/${dataset}"
+    TRAIN_FILE=${DATA_DIR}/train.csv
+    DEV_FILE=${DATA_DIR}/dev.csv
+    LOG_FILE="./result/cnn.csv"
+
+    CUDA_VISIBLE_DEVICES=${CUDA} python ../model/train_nn.py --dataset ${DATA_DIR} --model CNN --num_epoch 10 --log_file ${LOG_FILE}
+done
