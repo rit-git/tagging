fbf52ed8adbae6ee8a7bf5756b3dd1a064d4ea81
lijinf2 <jinfeng@megagon.ai>
2020-02-11 01:06:00 +0000

    fix lr

--- script/lr.sh
+++ script/lr.sh
@@ -1,5 +1,5 @@
 mkdir result
-echo "Dataset,precision,recall,f1,train_time(s),test_time(s)" > result/cnn.csv
+echo "Dataset,precision,recall,f1,train_time(s),test_time(s)" > result/lr.csv
 for dataset in "SUGG" 
 do
     SEED=12
@@ -7,7 +7,7 @@ do
     DATA_DIR="../data/${dataset}"
     TRAIN_FILE=${DATA_DIR}/train.csv
     DEV_FILE=${DATA_DIR}/dev.csv
-    LOG_FILE="./result/lr.txt"
+    LOG_FILE="./result/lr.csv"
 
     python ../model/lr.py ${TRAIN_FILE} ${DEV_FILE} ${LOG_FILE} ${SEED}
 done
