4bc2f4b8e1b119f09ab8b8617f14d0e71f00bba7
jinfeng <jinfeng@megagon.ai>
2020-07-16 01:03:32 +0000

    improve

--- appendix/auc_accuracy/svm_metric.sh
+++ appendix/auc_accuracy/svm_metric.sh
@@ -1,9 +1,9 @@
 mkdir result
 #echo "Dataset,precision,recall,f1, auc, accuracy" > result/lr.csv
-for dataset in "semeval20_task6" 
+for dataset in "SUGG" 
 #for dataset in "SUGG" "HOTEL" "SENT" "PARA" "FUNNY_400K" "HOMO" "HETER" "TV" "BOOK_400K" "EVAL" "REQ" "FACT" "REF" "QUOTE" "ARGUE" "SUPPORT" "AGAINST" "AMAZON_400K" "YELP" "FUNNY_STAR" "BOOK_STAR" 
 do
-    DATA_DIR="../../../tip/data/${dataset}"
+    DATA_DIR="../../data/${dataset}"
     SEED=12
     echo ${dataset}
     TRAIN_FILE=${DATA_DIR}/train.csv
