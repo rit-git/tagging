88817a529f304f8dd7ca44e440e29c450c33c4c0
lijinf2 <jinfeng@megagon.ai>
2020-08-02 18:33:17 +0000

    update bert script

--- script/bert.sh
+++ script/bert.sh
@@ -1,4 +1,4 @@
-CUDA=1
+CUDA=3
 mkdir result
 echo "Dataset,precision,recall,f1,train_time(s),test_time(s)" > result/bert.csv
 for dataset in "SUGG" 
@@ -24,7 +24,7 @@ do
         --max_seq_length 128 \
         --train_batch_size 32 \
         --learning_rate 2e-5 \
-        --num_train_epochs 5.0 \
+        --num_train_epochs 3.0 \
         --output_dir $LOG_DIR \
         --log_file_path $LOG_FILE_PATH
 
