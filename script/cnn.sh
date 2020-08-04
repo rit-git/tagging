77618827b2744e8c96a8a759530cc4f1f1604a28
lijinf2 <jinfeng@megagon.ai>
2020-08-04 04:58:38 +0000

    improve scripts

--- script/cnn.sh
+++ script/cnn.sh
@@ -1,4 +1,4 @@
-CUDA=1
+CUDA=3
 mkdir result
 echo "Dataset,precision,recall,f1,train_time(s),test_time(s)" > result/cnn.csv
 for dataset in "SUGG" 
@@ -9,5 +9,6 @@ do
     DEV_FILE=${DATA_DIR}/dev.csv
     LOG_FILE="./result/cnn.csv"
 
-    CUDA_VISIBLE_DEVICES=${CUDA} python ../model/train_nn.py --dataset ${DATA_DIR} --model CNN --num_epoch 10 --log_file ${LOG_FILE}
+    # AMAZON uses up memory. Append parameters: --glove 6B --emb_size 100 
+    CUDA_VISIBLE_DEVICES=${CUDA} python ../model/train_nn.py --dataset ${DATA_DIR} --model CNN --num_epoch 10 --log_file ${LOG_FILE} 
 done
--- script/lstm.sh
+++ script/lstm.sh
@@ -1,13 +1,14 @@
-CUDA=1
+CUDA=3
 mkdir result
 echo "Dataset,precision,recall,f1,train_time(s),test_time(s)" > result/lstm.csv
 for dataset in "SUGG" 
 do
     echo ${dataset}
-    DATA_DIR="../data/${dataset}"
+    DATA_DIR="../../tip/data/${dataset}"
     TRAIN_FILE=${DATA_DIR}/train.csv
     DEV_FILE=${DATA_DIR}/dev.csv
     LOG_FILE="./result/lstm.csv"
 
+    # AMAZON uses up memory. Append parameters: --glove 6B --emb_size 100 
     CUDA_VISIBLE_DEVICES=${CUDA} python ../model/train_nn.py --dataset ${DATA_DIR} --model LSTM --num_epoch 10 --log_file ${LOG_FILE}
 done
